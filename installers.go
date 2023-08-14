fc7251b4c9e08cd01ee64b0d11c8374a28112c0f
André Morgado <andremorgado.work@gmail.com>
2023-08-14 18:54:12 +0100

    feat: added overwrite zshrc cfg file functionality | added installation of zsh-autosuggestions and syntax highlighting

--- installers.go
+++ installers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
@@ -44,5 +45,34 @@ func installZshAndOhMyZsh(m model) (string, error) {
 		return "", fmt.Errorf("failed to install Oh My Zsh: %w", err)
 	}
 
-	return "Zsh and Oh My Zsh installed successfully.", nil
+	// Clone the setupjunkie-dotfiles repo
+	tmpDir := "/tmp/setupjunkie-dotfiles"
+	cmd = exec.Command("git", "clone", "https://github.com/andremore/setupjunkie-dotfiles.git", tmpDir)
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("failed to clone setupjunkie-dotfiles: %w", err)
+	}
+
+    // Overwrite the .zshrc file
+    homeDir, err := os.UserHomeDir()
+    if err != nil {
+        return "", fmt.Errorf("failed to get user's home directory: %w", err)
+    }
+    cmd = exec.Command("sudo", "cp", tmpDir+"/.zshrc", homeDir+"/.zshrc")
+    if err := cmd.Run(); err != nil {
+        return "", fmt.Errorf("failed to overwrite .zshrc: %w", err)
+    }
+
+	// Install zsh-autosuggestions
+    cmd = exec.Command("git", "clone", "https://github.com/zsh-users/zsh-autosuggestions", fmt.Sprintf("%s/.oh-my-zsh/custom/plugins/zsh-autosuggestions", homeDir))
+    if err := cmd.Run(); err != nil {
+        return "", fmt.Errorf("failed to install zsh-autosuggestions: %w", err)
+    }
+
+    // Install zsh-syntax-highlighting
+    cmd = exec.Command("git", "clone", "https://github.com/zsh-users/zsh-syntax-highlighting.git", fmt.Sprintf("%s/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting", homeDir))
+    if err := cmd.Run(); err != nil {
+        return "", fmt.Errorf("failed to install zsh-syntax-highlighting: %w", err)
+    }
+
+	return "Zsh, Oh My Zsh, and .zshrc setup completed.", nil
 }
